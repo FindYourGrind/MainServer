a06c2fcf5f6dc97f6997f3fa297951d9d8b55831
IT <igortram@gmail.com>
2017-05-26 10:12:03 +0300

    Host Configuration

--- client/config/index.js
+++ client/config/index.js
@@ -29,7 +29,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
         '/api': {
-            target: 'http://localhost:80',
+            target: 'http://localhost:3000',
             changeOrigin: true
         },
     },
--- client/src/webSocket.js
+++ client/src/webSocket.js
@@ -9,7 +9,8 @@ let ws = {
     io: IO,
     connect: function (options) {
         let me = this;
-        let host = me.protocol + '://localhost:80';
+        //let host = me.protocol + '://localhost:3000';
+        let host = '/';
 
         if (me.isConnectionExists === false) {
             Vue.use(VueWebsocket, host, options);
